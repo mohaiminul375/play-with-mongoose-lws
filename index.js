7163677f2138e7cb5a7c598f8f8ebe5029364f7e
Mohaiminul375 <mohaiminul375@gmail.com>
2025-04-04 20:12:14 +0600

    create todo routes

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const todoHandler = require('./routeHandler/todoHandler')
+
+
+//express app initialization
 const app = express();
 app.use(express.json());
+
+
 // Db connects with mongoose
+mongoose.connect('mongodb://localhost/todos')
+    .then(() => console.log('connect to mongoose'))
+    .catch(err => console.log(err))
 
-function errorHandler(err, rq, res, next) {
-    if (res.headerSent) {
-        return next(err)
-    }
-    res.status(500).json({ error: err })
-}
 
-app.listen(8000,()=>{
+app.use('/todo', todoHandler)
+
+// function errorHandler(err, rq, res, next) {
+//     if (res.headerSent) {
+//         return next(err)
+//     }
+//     res.status(500).json({ error: err })
+// }
+
+app.listen(8000, () => {
     console.log('app running on 8000')
 })
---
+++ routeHandler/todoHandler.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+
+// get all todos
+router.get('/', async (req, res) => {
+
+})
+
+// get todo by id
+router.get('/:id', async (req, res) => {
+
+})
+//  POST A TODO     
+router.post('/', async (req, res) => {
+
+})
+// post multiple todd
+router.post('/all', async (req, res) => {
+
+})
+// Put Todo
+router.put('/:id', async (req, res) => {
+
+})
+// Delete TODO
+router.delete('/:id', async (req, res) => {
+
+})
+
+module.exports = router;
